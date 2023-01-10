ad42cc6b96c857fe63c153c158d73ef451dcef05
Galdea <gabriel.aldea@gmail.com>
2023-01-10 23:03:17 +0000

    need to try APIs on Insomnia

--- src/app.py
+++ src/app.py
@@ -9,6 +9,8 @@ from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
 from models import db, User
+from flask import jsonify, request
+from models import db, User, Favorites, Character, Planet
 #from models import Person
 
 app = Flask(__name__)
@@ -44,6 +46,69 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@app.route('/users/', methods=['GET'])
+def get_users():
+    users = User.query.all()
+    return jsonify([u.serialize() for u in users]), 200
+
+@app.route('/users/<int:user_id>', methods=['GET'])
+def get_user(user_id):
+    user = User.query.filter_by(id=user_id).first()
+    return jsonify(user.serialize()), 200
+
+@app.route('/users/', methods=['POST'])
+def create_user():
+    firstname = request.json.get('firstname')
+    lastname = request.json.get('lastname')
+    email = request.json.get('email')
+    user = User(firstname=firstname, lastname=lastname, email=email)
+    db.session.add(user)
+    db.session.commit()
+    return jsonify(user.serialize()), 201
+
+@app.route('/favorites/', methods=['GET'])
+def get_favorites():
+    favorites = Favorites.query.all()
+    return jsonify([f.serialize() for f in favorites]), 200
+
+@app.route('/favorites/<int:favorite_id>', methods=['GET'])
+def get_favorite(favorite_id):
+    favorite = Favorites.query.filter_by(id=favorite_id).first()
+    return jsonify(favorite.serialize()), 200
+
+@app.route('/favorites/', methods=['POST'])
+def create_favorite():
+    user_id = request.json.get('user_id')
+    character_id = request.json.get('character_id')
+    planet_id = request.json.get('planet_id')
+    favorite = Favorites(user_id=user_id, character_id=character_id, planet_id=planet_id)
+    db.session.add(favorite)
+    db.session.commit()
+    return jsonify(favorite.serialize()), 201
+
+@app.route('/characters/', methods=['GET'])
+def get_characters():
+    characters = Character.query.all()
+    return jsonify([c.serialize() for c in characters]), 200
+
+@app.route('/characters/<int:character_id>', methods=['GET'])
+def get_character(character_id):
+    character = Character.query.filter_by(id=character_id).first()
+    return jsonify(character.serialize()), 200
+
+@app.route('/planets/', methods=['GET'])
+def get_planets():
+    planets = Planet.query.all()
+    return jsonify([p.serialize() for p in planets]), 200
+
+@app.route('/planets/<int:planet_id>', methods=['GET'])
+def get_planet(planet_id):
+    planet = Planet.query.filter_by(id=planet_id).first()
+    return jsonify(planet.serialize()), 200
+
+
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -1,19 +1,114 @@
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 from flask_sqlalchemy import SQLAlchemy
 
+engine = create_engine('sqlite:///starwars.db', echo=True) 
 db = SQLAlchemy()
+Base = db.Model
+Session = sessionmaker(bind=engine)
+session = Session()
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+class User(Base):
+    __tablename__ = 'Users'
+    id = Column(Integer, primary_key=True)
+    firstname = Column(String)
+    lastname = Column(String)
+    email = Column(String)
 
     def __repr__(self):
-        return '<User %r>' % self.username
-
+        return '<User {} {}>'.format(self.firstname, self.lastname)
+    
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
+            "firstname": self.firstname,
+            "lastname": self.lastname,
+            "email": self.email
         }
+
+class Favorites(Base):
+    __tablename__ = 'Favorites'
+    user_id = Column(Integer, ForeignKey('Users.id'), primary_key=True)
+    character_id = Column(Integer, ForeignKey('Characters.id'), primary_key=True)
+    planet_id = Column(Integer, ForeignKey('Planets.id'), primary_key=True)
+    character = relationship("Character")
+    planet = relationship("Planet")
+
+    def __repr__(self):
+        return '<Favorites {} {}>'.format(self.character.name, self.planet.name)
+    
+    def serialize(self):
+        return {
+            "user_id": self.user_id,
+            "character_id": self.character_id,
+            "planet_id": self.planet_id,
+            "character": self.character.serialize(),
+            "planet": self.planet.serialize()
+        }
+
+class Character(Base):
+    __tablename__ = 'Characters'
+    id = Column(Integer, primary_key=True)
+    mass = Column(Integer)
+    height = Column(Integer)
+    hair_color = Column(String)
+    skin_color = Column(String)
+    eye_color = Column(String)
+    birth_year = Column(String)
+    species = Column(String)
+    homeworld = Column(String)
+    gender = Column(String)
+    name = Column(String)
+
+    def __repr__(self):
+        return '<Character {}>'.format(self.name)
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "mass": self.mass,
+            "height": self.height,
+            "hair_color": self.hair_color,
+            "skin_color": self.skin_color,
+            "eye_color": self.eye_color,
+            "birth_year": self.birth_year,
+            "species": self.species,
+            "homeworld": self.homeworld,
+            "gender": self.gender,
+            "name": self.name
+        }
+
+class Planet(Base):
+    __tablename__ = 'Planets'
+    id = Column(Integer, primary_key=True)
+    diameter = Column(Integer)
+    rotation_period = Column(Integer)
+    orbital_period = Column(Integer)
+    gravity = Column(String)
+    population = Column(Integer)
+    climate = Column(String)
+    terrain = Column(String)
+    surface_water = Column(Integer)
+    name = Column(String)
+    url = Column(String)
+
+    def __repr__(self):
+        return '<Planet {}>'.format(self.name)
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "diameter": self.diameter,
+            "rotation_period": self.rotation_period,
+            "orbital_period": self.orbital_period,
+            "gravity": self.gravity,
+            "population": self.population,
+            "climate": self.climate,
+            "terrain": self.terrain,
+            "surface_water": self.surface_water,
+            "name": self.name,
+            "url": self.url
+        }
+
+Base.metadata.create_all(engine)
